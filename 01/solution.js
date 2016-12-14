b578afbe6613c4aa63047eb0e3c1da34acad3db1
David Crocker <mail@djcrocker.com>
2016-12-14 18:11:39 -0500

    README and day 01 solution

---
+++ 01/solution.js
@@ -0,0 +1,67 @@
+var fs = require('fs');
+
+var directions = {
+  N: {
+    L: 'W',
+    R: 'E',
+    axis: 'y',
+    dist: 1
+  },
+  E: {
+    L: 'N',
+    R: 'S',
+    axis: 'x',
+    dist: 1
+  },
+  S: {
+    L: 'E',
+    R: 'W',
+    axis: 'y',
+    dist: -1
+  },
+  W: {
+    L: 'S',
+    R: 'N',
+    axis: 'x',
+    dist: -1
+  }
+}
+
+var state = {
+  direction: directions.N,
+  x: 0,
+  y: 0
+}
+
+var visited = { '0': {'0': true}};
+var found = false;
+
+fs.readFile('./input.txt', 'utf8', function(err, data) {
+  var steps = data.trim().split(', ').map(function(step) {
+    return {
+      direction: step.substring(0, 1),
+      distance:  parseInt(step.substring(1))
+    };
+  });
+  var x = 0;
+  var y = 0;
+  steps.forEach(function(step) {
+    state.direction = directions[state.direction[step.direction]];
+    while(step.distance !== 0) {
+      state[state.direction.axis] += state.direction.dist;
+      step.distance--;
+      if(!found) {
+        visited[state.x] = visited[state.x] || {};
+        if(visited[state.x][state.y]) {
+          found = true;
+          console.log(state);
+          console.log(Math.abs(state.x) + Math.abs(state.y));
+        } else {
+          visited[state.x][state.y] = true;
+        }
+      }
+    }
+  });
+  console.log(state);
+  console.log(Math.abs(state.x) + Math.abs(state.y));
+});
---
+++ README.md
@@ -0,0 +1,2 @@
+# advent2016
+My [Advent of Code 2016](http://adventofcode.com/2016/) solutions
